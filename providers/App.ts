36fa0178cfa916f4921e253333512da5082b2339
rupesh <rupesh.patil741@gmail.com>
2023-10-20 18:05:21 +0530

    init app start

--- providers/App.ts
+++ providers/App.ts
@@ -1,9 +1,26 @@
 
 import * as express from 'express';
-
+import {userHandler, nftHandler} from './suiController'
 class App {
-    startServer(){
-            
+    StartServer(){
+        const port: number = 3002;
 
+		// Registering Exception / Error Handlers
+		const exp = express.application
+        const handler = new userHandler();
+        const nft = new nftHandler();
+        exp.use("/addUser", handler.addUser)
+        exp.use("/createUser", handler.createUser)
+        exp.use("/createNftByUser", nft.createNftOnNetwork)
+        exp.use("/getNftByUser", handler.getNfts)
+
+
+		exp.listen(port, () => {
+			return console.log('\x1b[33m%s\x1b[0m', `Server :: Running @ 'http://localhost:${port}'`);
+		}).on('error', (_error) => {
+			return console.log('Error: ', _error.message);
+		});
     }
 }
+
+export default new App;
---
+++ providers/suiController.ts
@@ -0,0 +1,27 @@
+
+
+
+
+
+export class userHandler {
+    addUser(){
+        // user object create
+        const user = new User();
+        // params from the req
+        
+    }
+
+    createUser(){
+
+    }
+
+    getNfts(){
+
+    }
+}
+
+export class nftHandler {
+    createNftOnNetwork(){   
+
+    }
+}
